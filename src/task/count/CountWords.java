ad84b17a99e0a5f3c0c8b923fe68aedc54e05446
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2022-04-19 19:24:07 +0300

    CountWords

--- src/task/count/CountWords.java
+++ src/task/count/CountWords.java
@@ -1,11 +1,17 @@
 package task.count;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Подсчет количества повторений слов.
- *
+ * <p>
  * Пример:
  * А В АА ВВ А ВВ А
- *
+ * <p>
  * Результат:
  * А - 3
  * В - 1
@@ -15,5 +21,23 @@ package task.count;
  * @author Vladimir Ivanov (ivanov.vladimir.l@gmail.com)
  */
 public class CountWords {
+	public static void main(String[] args) {
+		List<String> input = List.of("А", "В", "АА", "ВВ", "А", "ВВ", "А");
+		Map<String, Integer> result = countWords(input);
+		System.out.println(result);
+	}
+
+	// Сложность
+	// алг - O(n) - мин
+	// память - O(n) - мин
+	private static Map<String, Integer> countWords(List<String> input) {
+		Map<String, Integer> result = new LinkedHashMap<>();
+		for (String value : input) {
+			int count = result.getOrDefault(value, 0);
+			count++;
+			result.put(value, count);
+		}
+		return result;
+	}
 
 }
